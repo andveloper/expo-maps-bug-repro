7d76d78f405c97357e511bfd1ec70a28ffd9b211
Andreas Pelligra <18751616+andveloper@users.noreply.github.com>
2025-07-16 21:52:46 +0200

    Add location permission buttons

--- App.js
+++ App.js
@@ -1,6 +1,25 @@
-import { AppleMaps } from "expo-maps";
-import { StyleSheet } from "react-native";
+import {
+  AppleMaps,
+  getPermissionsAsync,
+  requestPermissionsAsync,
+} from "expo-maps";
+import { Button, StyleSheet, View } from "react-native";
 
 export default function App() {
-  return <AppleMaps.View style={StyleSheet.absoluteFill} />;
+  return (
+    <View style={{ flex: 1 }}>
+      <AppleMaps.View style={{ flex: 1 }} />
+
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <Button
+          title="requestPermissionsAsync"
+          onPress={() => requestPermissionsAsync()}
+        />
+        <Button
+          title="getPermissionsAsync"
+          onPress={() => getPermissionsAsync()}
+        />
+      </View>
+    </View>
+  );
 }
